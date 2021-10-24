01e3b56172477b3c8e1a3572e9a656664b2cb357
Sam <samuel.grulich@gmail.com>
2021-10-24 23:41:37 +0200

    V 1.0 discord bot
    bot functionality implemented
    agent added functionality

--- agent.py
+++ agent.py
@@ -3,39 +3,93 @@ import time
 
 from datetime import datetime
 
-PAGE = 'https://howrare.is/' 
+
 TIME_FMT = '%S:%M:%H,%d-%m-%Y'
 
 
+class Timer:
+    def __init__(self) -> None:
+        self.start = time.time_ns()
+
+    @property
+    def stop(self) -> int():
+        '''returns time(int) ns'''
+        now = time.time_ns()
+
+        return now - self.start
+
+
+
 class Agent:
-    def __init__(self, *args) -> None:
+    def __init__(self, id, *args, **kwargs) -> None:
+        self.id = id
+        
         self.snapshots = []
+        self.filtered_snapshots = []
         self.nft_list = []
+        self.filtered_nft_list = []
 
-        if not len(args) > 1:
-            self.collection_url = args[0]
-            self.page_url = PAGE
-            return
+        self.filters = kwargs
 
-        collection = args[0]
-        querries = args[1:]
+        self.url = self._args_handler(args)
 
-        collection = collection\
-            .replace(PAGE, '')\
-            .split('/')[0]
+        if len(self.url):
+            raw_html = self.request_collection(self.url)
+            self.parse_NFTs(raw_html)
+            self.filter_snapshot(**kwargs)
 
-        collection_url = PAGE + collection + '/'
 
-        if querries:
-            if type(querries) is str:
-                collection_url += querries
+    def set_url(self, *args) -> None:
+        self.url = self._args_handler(args)
 
-        self.page_url = PAGE
-        self.collection_url = PAGE + collection
+
+    def _args_handler(self, args) -> str:
+        '''
+        Inputed arguments are convereted to url string \n
+        reutrns url string
+        '''
+        def add_slash(string: str) -> str:
+            last_char = string[-1]
+
+            if not last_char == '/':
+                string + '/'
+
+            #return string[:]
+
+        len_ = len(args)
+        url = ''
+
+        if len_ > 1:
+            page_url, options = args[0], args[1:]
+            options_string = "&".join(options)
+
+            add_slash(page_url)
+            url = f'{page_url}?{options_string}'
+        elif len_ == 1:
+            url = args[0]
+
+        return url
+
 
     # REQUESTING
     #region
-    def evaluate_request(self, status_code: int, type = 'Collection') -> bool:
+    def request_collection(self, url: str) -> str:
+        '''
+        Requests url, evaluates result \n
+        returns html_body(string) else empty(string)
+        '''
+        try:
+            collection_response = requests.get(url)
+            collection_valid = self._evaluate_request(collection_response.status_code)
+        except:
+            collection_valid = self._evaluate_request(0)
+        if not collection_valid:
+            return ''
+
+        return collection_response.text
+    
+    
+    def _evaluate_request(self, status_code: int, type = 'Collection') -> bool:
         '''
         Checks validity of given response(or status_code), then logs result \n
         returns bool
@@ -49,52 +103,27 @@ class Agent:
             print(f'[EVALUATING] {type}, ❌ NOT valid')
 
         return validity
-
-
-    def request_collection(self, url: str) -> str:
-        '''
-        Checks if page is valid then collection page \n
-        returns string - empty if collection is not valid
-        '''
-        # TODO: group these into function
-        try:
-            page_response = requests.get(self.page_url)
-            page_valid = self.evaluate_request(page_response.status_code, 'Page')
-        except:
-            page_valid = self.evaluate_request(0, 'page')
-        if not page_valid: 
-            return ''
-        
-        try:
-            collection_response = requests.get(url)
-            collection_valid = self.evaluate_request(collection_response.status_code)
-        except:
-            collection_valid = self.evaluate_request(0)
-        if not collection_valid:
-            return ''
-
-        return collection_response.text
     #endregion
 
     # SNAPSHOT Manipulation 
     #region
-    def append_snaphsot(self, snapshot: list=[]):
-        print('[APPENDING]')
-        if not snapshot:
-            self.snapshots.append(self.nft_list)
+    def filter_snapshot(self, snapshot: list = [], **kwargs):
+        last_snap = []
+
+        if len(self.snapshots):
+            last_snap = self.snapshots[-1]
+        elif len(self.nft_list):
+            last_snap = self.nft_list
         else:
-            self.snapshots.append(snapshot)
-        
-
-
-    def compare_snapshot(self, snapshot: list = []):
-        if not len(self.snapshots):
             return
 
-        if not snapshot:
+        if not len(snapshot):
             snapshot = self.nft_list
 
-        last_snap = self.snapshots[-1]
+            if not len(snapshot):
+                print('no snapshot')
+                return
+
         last_time = last_snap[-1]
         last_snap = last_snap[:-1]
 
@@ -106,26 +135,54 @@ class Agent:
 
         f_time = last_time - snap_time
 
-        #last_snap, snap, f_time
+        filtered_snapshot = []
 
         for nft in snap:
-            if int(nft.Rank) < 1500:
-                print(nft.Rank)
-                print(nft.Price)
-                print(nft.__dict__.keys())
+            nft_dict = nft.__dict__
+            nft_keys = nft.__dict__.keys()
 
-            
+            condition = True
+
+            for key, value in kwargs.items():
+                if key in nft_keys:
+                    val = str(nft_dict[key])
+
+                    if 'SOL' in val:
+                        val = val.replace('SOL', '')
+
+                    condition = condition and float(val) < float(value)
+
+            if condition:
+                filtered_snapshot.append(nft)
+        
+        if len(filtered_snapshot):
+            filtered_snapshot.append(f_time)
+
+            self.filtered_nft_list = filtered_snapshot
+            self.filtered_snapshots.append(filtered_snapshot)
+    
+    
+    def _append_snaphsot(self, snapshot: list=[]):
+        print('[APPENDING]')
+        if not snapshot:
+            self.snapshots.append(self.nft_list)
+        else:
+            self.snapshots.append(snapshot)
     #endregion
 
     # REQUEST PARSING
-    def parse_NFTs(self, raw_page: str) -> list:
+    #region
+    def parse_NFTs(self, raw_page: str='') -> list:
         '''
         Takes in raw html of the page \n
         returns list/array of NFT objects
         '''
         
         if not raw_page:
-            return
+            raw_page = self.request_collection(self.url)
+
+            if not raw_page:
+                return
 
         a_split = raw_page.split('<a')
         a_split.pop(0)
@@ -133,11 +190,12 @@ class Agent:
         
         # only divs of NFTS do have this filter, yeah its quiet ugly
         a_split = list(filter(lambda x : 'class=\"m-2\" style=\"display: block;\"' in x, a_split))
-        a_split[-1], _ = a_split[-1].split('</a>')
+        a_split[-1] = a_split[-1].split('</a>')[0]
 
-        start = time.time_ns()
+        timer = Timer()
         print(f'[PARSING] Html to objects')
 
+        # actual parsing
         nft_list = []
         for raw_data_block in a_split:
             raw_data_block = '<a' + raw_data_block
@@ -146,13 +204,45 @@ class Agent:
 
             nft = NFT(raw_data_block)
             nft_list.append(nft)
-
-        end = time.time_ns()
-        print(f'[PARSING] Parsing done {(end - start) / 1e6}ms')
         nft_list.append(datetime.now().strftime(TIME_FMT))
+
+        print(f'[PARSING] Parsing done {timer.stop / 1e6}ms')
+
+        # saving
+        self._append_snaphsot(nft_list)
         self.nft_list = nft_list
         return nft_list
+    #endregion
 
+    # UPDATING
+    #region
+    def update(self):
+        print(self.url)
+        self.parse_NFTs()
+        self.filter_snapshot(**self.filters)
+
+
+    def compare(self, snapshot: list = []) -> list:
+        '''
+        returns list of changes
+        '''
+        changes = []
+
+        if not snapshot:
+            snapshot = self.filtered_nft_list
+
+        if not len(self.filtered_snapshots) >= 2:
+            print('Not enough snapshots')
+            return []
+
+        last_snapshot = self.filtered_snapshots[-2]
+
+        for nft, last_nft in zip(snapshot[:-1], last_snapshot[:-1]):
+            if not nft.data == last_nft.data:
+                changes.append((nft, last_nft))
+
+        return changes
+    #endregion
 
 
 class NFT:
@@ -164,51 +254,61 @@ class NFT:
         #   I would takde data about this NFT from it's detail page
         self.m_raw_data = raw_data
 
-        # Formating text, for easier manipulation
-        data = NFT.format_data(raw_data)
+        self.format_data()
+        
 
-        #split to individual lines
+    def format_data(self):
+        '''
+        !(Required: self.m_raw_data) \n
+        Parses NFT from raw data to attributes \n
+            attributes are then implemented as \
+            member_variables to this object
+        '''
+        # Formating text, for easier manipulation
+        if not self.m_raw_data:
+            print(f'There are no raw data in {self}')
+
+        data = NFT.remove_spaces(self.m_raw_data)
         lines = data.split(';--;\n')
 
-        args, args_html = [], []
-        #region Parsing args
-        def parse_arg(new_list, condition, func):
-            for line in lines:
-                if condition(line):
-                    new_list.append(func(line))
+        attribs = NFT._parse_attbribs(lambda x : not '<' in x, lambda x: x.replace('\"', '').split(':'), lines)
+        attribs_html = NFT._parse_attbribs(lambda x : '<' in x, lambda x : x.replace('<', '').replace('=', '').split('\"')[:2], lines)
 
-        # args without html tag
-        parse_arg(
-            args, 
-            lambda x : not '<' in x, 
-            lambda x: x.replace('\"', '').split(':') if not '<' in x else x
-        )
+        self._implement_attribs(attribs)
+        self._implement_attribs(attribs_html, 'm_')
 
-        # extract args from html tag
-        parse_arg(
-            args_html, 
-            lambda x : '<' in x, 
-            lambda x : x.replace('<', '').replace('=', '').split('\"')[:2] if '<' in x else x, 
-        )
-        #endregion
-        
-        #region Implement args as attributes
-        for attrib in args:
+
+    def _implement_attribs(self, attribs: list, prefix=''):
+        for attrib in attribs:
             if len(attrib) == 1: 
                 continue
 
-            setattr(self, attrib[0], attrib[1])
+            setattr(self, prefix+attrib[0], attrib[1])
 
-        # implement html_args as member attribs
-        for attrib in args_html:
-            if len(attrib) == 1: 
-                continue
+    @property
+    def data(self) -> str:
+        keys = self.__dict__.keys()
+        string = ''
 
-            setattr(self, 'm_'+attrib[0], attrib[1])
-        #endregion
+        for key in keys:
+            if not 'm_' == key[:2]:
+                string += f'{key}: {self.__dict__[key]}\n'
+
+        return string
+
 
     @staticmethod
-    def format_data(raw_data):
+    def _parse_attbribs(condition, func, lines: list) -> list:
+        output_list = []
+
+        for line in lines:
+            if condition(line):
+                output_list.append(func(line))
+
+        return output_list
+
+    @staticmethod
+    def remove_spaces(raw_data):
         data = raw_data.replace('<divclass="nft-token-listrounded">', '')
         data = data.replace('<divclass="nft-details">', '')
         data = data.replace('<strong>', '\"').replace('</strong>', '\"')
@@ -227,9 +327,9 @@ class NFT:
 
 
 
-url = 'https://howrare.is/solsnatchers/?for_sale=on&sort_by=price'
-a = Agent(url)
-raw_html = a.request_collection(url)
-a.parse_NFTs(raw_html)
-a.append_snaphsot()
-a.compare_snapshot()
+if __name__ == '__main__':
+    url = 'https://howrare.is/solsnatchers/?for_sale=on&sort_by=price'
+    a = Agent('John Doe', url, Rank=1500)
+    changes = a.compare()
+    # a.parse_NFTs()
+    # a.filter_snapshot(Rank=1500)
--- bot.py
+++ bot.py
@@ -1,25 +1,103 @@
-from discord import Status
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord import utils
 
-import agent as ag
+from agent import Agent
+
+from dotenv import dotenv_values
+
+from time import asctime
+
+
+config = dotenv_values(".env")
+CHANNEL = config['channel']
+BOT_KEY = config['bot_key']
+MAINLOOP_TIME = config['mainloop_time']
 
 
 
 client = commands.Bot(command_prefix='.')
+agents = []
 
 
-# Command functions
+# Functions
 #region
+def parse_kwargs(args) -> dict:
+    kwargs = {}
+    
+    for arg in args:
+        string = str(arg)
 
+        if '=' in string:
+            key, value = string.split('=', 1)
+            kwargs[key] = value
+
+    return kwargs
+
+
+async def create_agent(ctxt, id, **kwargs):
+    url = ''
+
+    if 'url' in kwargs.keys():
+        url = kwargs['url']
+        print(url)
+        del kwargs['url']
+    
+    a = Agent(id, url, **kwargs)
+
+    agents.append(a)
+    
+    await ctxt.send(f'Agent {id} created \n')
+
+    channel = utils.get(client.get_all_channels(), name=CHANNEL)
+
+    await channel.send('@here ------------------------')
+    await channel.send(f'>>> {asctime()}, agent: {a.id} snapshot: {len(a.filtered_nft_list[:-1])}')
+
+    for nft in a.filtered_nft_list[:-1]:
+        print(nft.data)
+        await channel.send(f'```: {nft.data} \n```')
+
+
+async def delete_agent(ctxt, id):
+    for index, agent in enumerate(agents):
+        if agent.id == id:
+            agents.pop(index)
+            
+            await ctxt.send(f'Bot {id} removed')
+            return
+
+
+async def update_agents(channel):
+    if not agents:
+        return
+
+    for agent in agents:
+        agent.update()
+        changes = agent.compare()
+
+        print(len(changes))
+        if len(changes):
+            await channel.send('@here ------------------------')
+            await channel.send(f'>>> {asctime()}, agent: {agent.id} changes: {len(changes)}')
+
+            for change in changes:
+                await channel.send(f'```original: {change[1].data} \n new: {change[1].data}```')
 
 
 #endregion
 
 
 
+# Binding
+#region
 @client.event
 async def on_ready():
     print('Bot ready')
+
+    channel = utils.get(client.get_all_channels(), name=CHANNEL)
+    
+    print('loop started')
+    main_loop.start(channel)
 
 
 @client.command(help='check bot\'s latency')
@@ -27,10 +105,21 @@ async def ping(ctxt):
     await ctxt.send(f'{int(client.latency * 1e3)}ms')
 
 
-@client.command()
-async def create(ctxt, id, **kwargs):
-    pass
+@client.command(help='''create agent "id" -"url" (filters)"name=max_value"''', aliases=['c'])
+async def create(ctxt, id, *args,):
+    kwargs = parse_kwargs(args)
+    await create_agent(ctxt, id, **kwargs)
 
 
+@client.command(help='''remove agent "id"''', aliases=['r', 'd'])
+async def delete(ctxt, id):
+    await delete_agent(ctxt, id)
+#endregion
 
-client.run('OTAxODA3MzQ3NTAzMDkxNzYz.YXVPig.KjC0-DDcNDWGZnD3k0VdvjvqQpY')
+
+@tasks.loop(seconds=int(MAINLOOP_TIME))
+async def main_loop(channel):
+    await update_agents(channel)
+    print('loop done')
+
+client.run(BOT_KEY)
